134db934d32fdf5a10d31310db0556ff39ac6954
Du Dang <ddang@infitrak.com>
2014-09-11 16:32:57 -0400

    Added the audit table structure

--- dev/tbl_zaudit.sql
+++ dev/tbl_zaudit.sql
@@ -1,4 +1,32 @@
 DELIMITER $$
+DROP TABLE IF EXISTS `zaudit`
+$$
+CREATE TABLE `zaudit` (
+  `audit_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) DEFAULT NULL,
+  `table_name` varchar(255) DEFAULT NULL,
+  `pk1` varchar(255) DEFAULT NULL,
+  `pk2` varchar(255) DEFAULT NULL,
+  `action` varchar(6) DEFAULT NULL COMMENT 'Values: insert|update|delete',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`audit_id`),
+  KEY `pk_index` (`table_name`,`pk1`,`pk2`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+$$
+DROP TABLE IF EXISTS `zaudit_meta`
+$$
+CREATE TABLE `zaudit_meta` (
+  `audit_meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `audit_id` bigint(20) unsigned NOT NULL,
+  `col_name` varchar(255) NOT NULL,
+  `old_value` longtext,
+  `new_value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`audit_meta_id`),
+  KEY `zaudit_meta_index` (`audit_id`,`col_name`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+$$
 DROP TABLE IF EXISTS `multi_key`
 $$
 CREATE TABLE `multi_key` (
