5cd1286363f5e5c3e6981ed4f1b56bc32aa603e1
Du Dang <ddang@infitrak.com>
2014-09-16 10:13:49 -0400

    Brought test tables out side to tbl_test_tables.sql

--- dev/tbl_zaudit.sql
+++ dev/tbl_zaudit.sql
@@ -27,29 +27,3 @@ CREATE TABLE `zaudit_meta` (
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 $$
-DROP TABLE IF EXISTS `multi_key`
-$$
-CREATE TABLE `multi_key` (
-  `first_id` int(11) NOT NULL,
-  `my second id` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `data` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ME_BLOB` longblob,
-  PRIMARY KEY (`first_id`,`my second id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-$$
-DROP TABLE IF EXISTS `test_data`
-$$
-
-CREATE TABLE `test_data` (
-  `my_id` int(10) unsigned NOT NULL,
-  `str` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `num` float DEFAULT NULL,
-  `dt` date DEFAULT NULL,
-  `ts` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `image` longblob,
-  PRIMARY KEY (`my_id`),
-  KEY `col_index` (`str`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-$$
